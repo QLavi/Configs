6c07f970f88890e36c0919ec02332b5b6bc87272
QLavi <qlavi@github.com>
2021-05-01 08:25:22 +0530

    removed unnecessary semi-colons

--- Scripts/FirstTime.sh
+++ Scripts/FirstTime.sh
@@ -10,34 +10,34 @@ function os_setup() {
 }
 
 function in_alacritty() {
-	sudo add-apt-repository ppa:aslatter/ppa;
-	sudo apt update;
-	sudo apt install alacritty;
-	cd;
+	sudo add-apt-repository ppa:aslatter/ppa
+	sudo apt update
+	sudo apt install alacritty
+	cd
 	mkdir ~/.config/alacritty
 	cp ~/Repos/Configs/Alacritty/alacritty.yml ~/.config/alacritty/
 }
 
 function in_popshell() {
 	sudo apt install node-typescript
-	cd;
-	mkdir Repos; cd Repos;
-	git clone https://github.com/pop-os/shell;
-	cd shell;
-	make local-install;
+	cd
+	mkdir Repos; cd Repos
+	git clone https://github.com/pop-os/shell
+	cd shell
+	make local-install
 }
 
 function in_utils() {
-	sudo apt install htop neofetch curl tree;
+	sudo apt install htop neofetch curl tree
 }
 
 function lang_utils() {
-	sudo apt install g++ gcc make;
+	sudo apt install g++ gcc make
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
 
 function in_editors() {
-	sudo apt install neovim;
+	sudo apt install neovim
 	
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -47,38 +47,38 @@ function in_editors() {
 
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo apt update;
-	sudo apt install sublime-text;
+	sudo apt update
+	sudo apt install sublime-text
 }
 
 function in_spotify_gimp() {
 	curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-	sudo apt update;
-	sudo apt install spotify-client gimp;
+	sudo apt update
+	sudo apt install spotify-client gimp
 }
 
 function in_JBToolbox() {
-	cd;
-	cd Repos;
-	git clone https://github.com/nagygergo/jetbrains-toolbox-install;
-	cd jetbrains-toolbox-install;
-	./jetbrains-toolbox.sh;
-	cd ..;
+	cd
+	cd Repos
+	git clone https://github.com/nagygergo/jetbrains-toolbox-install
+	cd jetbrains-toolbox-install
+	./jetbrains-toolbox.sh
+	cd ..
 	rm -rf jetbrains-toolbox-install
 }
 
 function main() {
-	update;
-	os_setup;
-	in_utils;
-	in_spotify_gimp;
-	in_alacritty;
-	lang_utils;
-	in_editors;
-	in_popshell;
+	update
+	os_setup
+	in_utils
+	in_spotify_gimp
+	in_alacritty
+	lang_utils
+	in_editors
+	in_popshell
 
-	in_JBToolbox;
-	sudo apt autoremove && sudo apt autoclean;
+	in_JBToolbox
+	sudo apt autoremove && sudo apt autoclean
 }
 main
